5b65e0130bdda118c996b825f527dde48a7eb93a
yyrrcc <music_v@naver.com>
2025-09-26 15:08:16 +0900

    카운트 미니프로젝트 완료

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import "./App.css";
+import { useState } from "react";
+import Viewer from "./components/Viewer";
+import Controller from "./components/Controller";
 
 function App() {
+  // 자식 컴포넌트에게 props 전달을 위해 부모 컴포넌트 App에서 선언
+  const [count, setCount] = useState(0);
+
+  const onClickCount = (value) => {
+    setCount(count + value);
+  };
+
   return (
     <div className="App">
-      <h1>시작</h1>
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer count={count} />
+      </section>
+      <section>
+        <Controller onClickCount={onClickCount} />
+      </section>
     </div>
   );
 }
