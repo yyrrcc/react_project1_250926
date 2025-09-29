8b7f51a204d627ce02028ddf69a674a82b3722e6
yyrrcc <music_v@naver.com>
2025-09-29 09:34:12 +0900

    코드 변경중..

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import Controller from "./components/Controller";
 import Even from "./components/Even";
 
 function App() {
-  // 자식 컴포넌트에게 props 전달을 위해 부모 컴포넌트 App에서 선언
+  // 0. 자식 컴포넌트에게 props 전달을 위해 부모 컴포넌트 App에서 선언
   const [count, setCount] = useState(0);
   const [text, setText] = useState("");
 
-  const onClickCount = (value) => {
+  const handleSetCount = (value) => {
     setCount(count + value);
   };
 
@@ -17,12 +17,15 @@ function App() {
     setText(e.target.value);
   };
 
-  // didMountRef : 처음 한 번은 건너뛰고 이후 업데이트 때만 코드가 돌게 하려는 플래그
+  // 3. didMountRef : 처음 한 번은 건너뛰고 이후 업데이트 때만 코드가 돌게 하려는 플래그
   const didMountRef = useRef(false);
-  // 함수 useEffect : 어떤 값이 변경될 때마다 특정 코드를 실행하는 리액트 훅
+
+  // 1. 함수 useEffect : 어떤 값이 변경될 때마다 특정 코드를 실행하는 리액트 훅
   // 랜더링 후에 발생하는 부작용을 처리하는 함수로 랜더링 후에 무조건 실행되는 함수임
   // useEffect(callback 콜백 함수, [deps] 의존성 배열)
   useEffect(() => {
+    // 3. 현재 didMountRef.current의 기본값 false -> !didMountRef.current = true -> if문 실행
+    // -> didMountRef.current = true로 바뀌고 그 다음부터는 else 블록만 실행되게 해주는 패턴
     if (!didMountRef.current) {
       didMountRef.current = true;
       return;
@@ -30,28 +33,29 @@ function App() {
       console.log("컴포넌트 업데이트!");
     }
   });
+
+  // 2. 마운트 될 때마다 + count,  text 변화 있을 때 실행됨
   // useEffect(() => {
-  //   console.log("업데이트 : ", text, count, " / 마운트 될 때도 실행됨");
+  //   console.log("마운트 됨! + 업데이트 : ", text, count);
   // }, [count, text]);
 
-  // 컴포넌트의 마운트 시점에 실행되는 코드
-  // useEffect에서 빈 배열을 전달하면 컴포넌트의 마운트 시점에만 콜백 함수를 실행
+  // 2. 의존성 배열을 빈 배열로 전달하면 최초로 마운트 될 때만 1번만 실행됨
   useEffect(() => {
-    console.log("컴포넌트 마운트");
+    console.log("컴포넌트 최초 마운트");
   }, []);
 
-  // 1초마다 콘솔에 문자열 출력
+  // 2. setInterval 함수 이용해서 1초마다 콘솔에 문자열 출력
   // useEffect(() => {
   //   setInterval(() => {
   //     console.log("깜빡");
-  //   }, 50000);
+  //   }, 1000);
   // });
 
-  // useEffect의 콜백 함수가 반환하는 함수를 클린업 함수. 콜백 함수를 다시 호출하기 전에 실행
+  // 4. useEffect의 콜백 함수가 반환하는 함수를 클린업 함수. 콜백 함수를 다시 호출하기 전에 실행
   // 따라서 컴포넌트를 렌더링할 때 마다 새 인터벌을 생성하고 기존 인터벌은 삭제
   useEffect(() => {
     const intervalID = setInterval(() => {
-      console.log("깜빡");
+      console.log("깜빡!");
     }, 1000);
     return () => {
       console.log("클린업");
@@ -70,7 +74,7 @@ function App() {
         {count % 2 === 0 && <Even />}
       </section>
       <section>
-        <Controller onClickCount={onClickCount} />
+        <Controller handleSetCount={handleSetCount} />
       </section>
     </div>
   );
--- src/components/Controller.js
+++ src/components/Controller.js
@@ -1,20 +1,20 @@
-const Controller = ({ onClickCount }) => {
+const Controller = ({ handleSetCount }) => {
   return (
     <div>
       {/*
       onClick에는 '함수'를 넣어야 하고,
-      onClick={onClickCount(-1)}는 '함수 결과'를 넣는 거라서 렌더하는 순간 바로 실행됨
-      onClick={() => onClickCount(-1)}처럼 “함수를 하나 더 만들어 넘겨서, 클릭될 때 실행”되게 해야 함
+      onClick={handleSetCount(-1)}는 '함수 결과'를 넣는 거라서 렌더하는 순간 바로 실행됨
+      onClick={() => handleSetCount(-1)}처럼 “함수를 하나 더 만들어 넘겨서, 클릭될 때 실행”되게 해야 함
 
       괄호가 붙으면 '지금 실행', 괄호가 없으면 '참조만 전달'
       이벤트 핸들러에는 항상 '참조'를 넣고, 인자가 필요하면 화살표 함수로 '나중에 실행'되게 감싸면 된다.
       */}
-      <button onClick={() => onClickCount(-1)}>-1</button>
-      <button onClick={() => onClickCount(-10)}>-10</button>
-      <button onClick={() => onClickCount(-100)}>-100</button>
-      <button onClick={() => onClickCount(+100)}>+100</button>
-      <button onClick={() => onClickCount(+10)}>+10</button>
-      <button onClick={() => onClickCount(+1)}>+1</button>
+      <button onClick={() => handleSetCount(-1)}>-1</button>
+      <button onClick={() => handleSetCount(-10)}>-10</button>
+      <button onClick={() => handleSetCount(-100)}>-100</button>
+      <button onClick={() => handleSetCount(+100)}>+100</button>
+      <button onClick={() => handleSetCount(+10)}>+10</button>
+      <button onClick={() => handleSetCount(+1)}>+1</button>
     </div>
   );
 };
